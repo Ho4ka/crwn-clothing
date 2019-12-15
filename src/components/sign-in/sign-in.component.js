bda6235301fb5fdc43aa03d052ac5ef3f726bdf2
Ho4ka <m.domaretsky@gmail.com>
2019-12-15 13:36:21 +0200

    Added sign in with google and handle sign in sign out case

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -52,9 +52,11 @@ class SignIn extends Component{
                         label="password"
                         handleChange={this.handleChange}
                     />
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={ signInWithGoogle }>Sign in with google</CustomButton>
-                </form>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>Sign in with google</CustomButton>
+                    </div>
+                   </form>
             </div>
         )
     }
