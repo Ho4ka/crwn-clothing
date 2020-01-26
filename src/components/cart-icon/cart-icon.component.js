0c9a92a2e4d33b469dbbe942405d719176789672
Ho4ka <m.domaretsky@gmail.com>
2020-01-26 22:21:05 +0200

    Added CartItem component + connected with redux

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -9,7 +9,7 @@ import { ReactComponent as ShoppingIcon } from "../../assets/11.2 shopping-bag.s
 import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden }) => (
-    <div className="cart-item" onClick={ toggleCartHidden }>
+    <div className="cart-icon" onClick={ toggleCartHidden }>
         <ShoppingIcon className="shopping-icon" />
         <span className="item-count">0</span>
     </div>
@@ -17,6 +17,6 @@ const CartIcon = ({ toggleCartHidden }) => (
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 
 export default connect(null, mapDispatchToProps)(CartIcon);
