7bf8e1e1724c9e093c1afba52b3da3614b1bd3fe
Ho4ka <m.domaretsky@gmail.com>
2020-02-25 22:39:24 +0200

    Added Checkout components and logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 
 import { connect } from "react-redux";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.action";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends Component {
   unsubscribeFromAuth = null;
@@ -43,6 +47,7 @@ class App extends Component {
             <Switch>
                 <Route  exact path="/" component={ HomePage }/>
                 <Route  path="/shop" component={ ShopPage }/>
+                <Route exact path="/checkout" component={ CheckoutPage }/>
                 <Route  exact
                         path="/signin"
                         render={() =>
@@ -56,8 +61,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
